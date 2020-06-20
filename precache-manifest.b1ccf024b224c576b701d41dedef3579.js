3707822da53b3b7f26f5f58f971258c644126e05
Suryaprakash Agarwal <suryaprakashagarwal245@gamil.com>
2020-06-20 12:30:38 +0530

    Updates

---
+++ precache-manifest.b1ccf024b224c576b701d41dedef3579.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76a4fd77296b8f08e551659f9e776d64",
+    "url": "/mnit-portal/index.html"
+  },
+  {
+    "revision": "4c346d4c7c79dfa81d4c",
+    "url": "/mnit-portal/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "e22695b462f6ed25c0d8",
+    "url": "/mnit-portal/static/css/main.9a7f3c17.chunk.css"
+  },
+  {
+    "revision": "4c346d4c7c79dfa81d4c",
+    "url": "/mnit-portal/static/js/2.8b438b35.chunk.js"
+  },
+  {
+    "revision": "3223ff019af3cdc1f320467fab225070",
+    "url": "/mnit-portal/static/js/2.8b438b35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e22695b462f6ed25c0d8",
+    "url": "/mnit-portal/static/js/main.11c9fa4b.chunk.js"
+  },
+  {
+    "revision": "945d5fc7060909826f3f",
+    "url": "/mnit-portal/static/js/runtime-main.df74e656.js"
+  }
+]);
